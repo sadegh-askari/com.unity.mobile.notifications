67ba1cd9e1d6ec31d718a692065d70b4643b7eb5
Sadegh Askari <sm.sadegh.askari@gmail.com>
2022-07-07 15:01:45 +0430

    add check for notification enable

--- com.unity.mobile.notifications/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationManager.java
+++ com.unity.mobile.notifications/Runtime/Android/Plugins/com/unity/androidnotifications/UnityNotificationManager.java
@@ -178,6 +178,46 @@ public class UnityNotificationManager extends BroadcastReceiver {
     {
         return String.format("unity_notification_channel_%s", id);
     }
+    
+    
+    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+    public static boolean isNotificationEnabled(Context context) {
+    
+            AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+    
+            ApplicationInfo appInfo = context.getApplicationInfo();
+    
+            String pkg = context.getApplicationContext().getPackageName();
+    
+            int uid = appInfo.uid;
+    
+            Class appOpsClass = null; /* Context.APP_OPS_MANAGER */
+    
+            try {
+    
+                appOpsClass = Class.forName(AppOpsManager.class.getName());
+    
+                Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+    
+                Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+                int value = (int)opPostNotificationValue.get(Integer.class);
+    
+                return ((int)checkOpNoThrowMethod.invoke(mAppOps,value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+    
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
 
     // Get a notification channel by id.
     // This function will only be called for devices which are low than Android O.
